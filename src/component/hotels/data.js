93608188c33fc659a04f15537f39a37c9d303b34
abderehmen02 <ilahalaha0987@gmail.com>
2023-02-06 21:47:31 +0100

    adding hotels features

--- src/component/hotels/data.js
+++ src/component/hotels/data.js
@@ -1,83 +1,125 @@
 import axios from 'axios' ;
 import "dotenv"
-export default async ( lat='51.509865' , lon='-0.118092' , priceMax='10000', priceMin='0'  ) =>{
-var options = {
+
+
+
+
+
+// export default async ( lat='51.509865' , lon='-0.118092' , priceMax='10000', priceMin='0'  ) =>{
+
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://priceline-com.p.rapidapi.com/hotels/city/nearby/37.788719679657554/-122.40057774847898',
+//   params: {radius: '100'},
+//   headers: {
+//     'X-RapidAPI-Key': '755a496f99mshcc845d2e84994e3p1a6c8djsn13bf2bce5c56',
+//     'X-RapidAPI-Host': 'priceline-com.p.rapidapi.com'
+//   }
+// };
+
+
+// axios.get('https://priceline-com.p.rapidapi.com/hotels/city/nearby/37.788719679657554/-122.40057774847898'  , {
+//   headers: {
+//     'X-RapidAPI-Key': '755a496f99mshcc845d2e84994e3p1a6c8djsn13bf2bce5c56',
+//     'X-RapidAPI-Host': 'priceline-com.p.rapidapi.com'
+//   }
+
+// }).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+
+// axios.request(options).then(function (response) {
+//   console.log(response.data)
+// 	console.log(response.data);
+// }).catch(function (error) {
+
+// });
+// return null
+// }
+
+
+
+
+
+// export const HotelReviews = async (geoid)=>{
+// const options = {
+//   method: 'GET',
+//   url: 'https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete',
+//   params: {query: 'eiffel tower', lang: 'en_US', units: 'km'},
+//   headers: {
+//     'X-RapidAPI-Key': '755a496f99mshcc845d2e84994e3p1a6c8djsn13bf2bce5c56',
+//     'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+//   console.log(response.data)
+// 	console.log(response.data);
+// }).catch(function (error) {
+//   console.log("post error")
+// 	console.error(error);
+// });
+// return null
+// }
+// export const HotelPhotos = async (id)=>{  
+//   var options = {
+//   method: 'GET',
+//   url: 'https://hotels-com-provider.p.rapidapi.com/v1/hotels/photos',
+//   params: {hotel_id: id},
+//   headers: {
+//     'x-rapidapi-host': process.env.REACT_APP_API_HOST,
+//     'x-rapidapi-key': process.env.REACT_APP_API_KEY
+//   }
+// };
+// //pie pie
+// const response = await axios.request(options)
+// return response.data
+// }
+// export const HotelDetais = async (id)=>{
+//   var options = {
+//   method: 'GET',
+//   url: 'https://hotels-com-provider.p.rapidapi.com/v1/hotels/booking-details',
+//   params: {
+//     adults_number: '1',
+//     checkin_date: '2022-03-26',
+//     locale: 'en_US',
+//     currency: 'USD',
+//     hotel_id: id,
+//     checkout_date: '2022-03-27',
+//     children_ages: '4,0'
+//   },
+//   headers: {
+//     'x-rapidapi-host': process.env.REACT_APP_API_HOST,
+//     'x-rapidapi-key': process.env.REACT_APP_API_KEY
+//   }
+// };
+
+// const response = await axios.request(options)
+// return response.data
+// }
+
+
+
+export const nearbyHotels = async (lat , long)=>{
+  try{
+const options = {
   method: 'GET',
-  url: 'https://hotels-com-provider.p.rapidapi.com/v1/hotels/nearby',
-  params: {
-    latitude: lat,
-    currency: 'USD',
-    longitude: lon ,
-    checkout_date: '2022-03-27',
-    sort_order: 'STAR_RATING_HIGHEST_FIRST',
-    checkin_date: '2022-03-26',
-    adults_number: '1',
-    locale: 'en_US',
-    guest_rating_min: '4',
-    star_rating_ids: '3,4,5',
-    children_ages: '4,0,15',
-    page_number: '1',
-    price_min: priceMin,
-    accommodation_ids: '20,8,15,5,1',
-    theme_ids: '14,27,25',
-    price_max: priceMax,
-    amenity_ids: '527,2063'
-  },
+  url: 'https://booking-com.p.rapidapi.com/v1/hotels/nearby-cities',
+  params: {longitude: lat, locale: 'en-gb', latitude: long},
   headers: {
-    'x-rapidapi-host': process.env.REACT_APP_API_HOST,
-    'x-rapidapi-key': process.env.REACT_APP_API_KEY
-  }
-};
-const res = await  axios.request(options)
-return res.data
-} 
-export const HotelReviews = async (geoid)=>{
-  var options = {
-  method: 'GET',
-  url: 'https://hotels-com-provider.p.rapidapi.com/v1/hotels/reviews',
-  params: {locale: 'en_US', hotel_id: geoid , page_number: '1'},
-  headers: {
-    'x-rapidapi-host': process.env.REACT_APP_API_HOST,
-    'x-rapidapi-key': process.env.REACT_APP_API_KEY
+    'X-RapidAPI-Key': '16d419c209mshc5e16371018746ap1939f1jsn36c21016982a',
+    'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
   }
 };
 
-const response = await axios.request(options)
-return response.data
-}
-export const HotelPhotos = async (id)=>{  
-  var options = {
-  method: 'GET',
-  url: 'https://hotels-com-provider.p.rapidapi.com/v1/hotels/photos',
-  params: {hotel_id: id},
-  headers: {
-    'x-rapidapi-host': process.env.REACT_APP_API_HOST,
-    'x-rapidapi-key': process.env.REACT_APP_API_KEY
+const res = await axios.request(options)
+  return ({data : res.data})
   }
-};
-//pie pie
-const response = await axios.request(options)
-return response.data
+catch (error){
+  return ({error })
 }
-export const HotelDetais = async (id)=>{
-  var options = {
-  method: 'GET',
-  url: 'https://hotels-com-provider.p.rapidapi.com/v1/hotels/booking-details',
-  params: {
-    adults_number: '1',
-    checkin_date: '2022-03-26',
-    locale: 'en_US',
-    currency: 'USD',
-    hotel_id: id,
-    checkout_date: '2022-03-27',
-    children_ages: '4,0'
-  },
-  headers: {
-    'x-rapidapi-host': process.env.REACT_APP_API_HOST,
-    'x-rapidapi-key': process.env.REACT_APP_API_KEY
-  }
-};
-
-const response = await axios.request(options)
-return response.data
 }
--- src/component/hotels/index.js
+++ src/component/hotels/index.js
@@ -2,30 +2,106 @@ import React , {useState , useEffect , useReducer , useCallback } from 'react'
 import GoogleMapReact from 'google-map-react';
 import  { StarHalf , StarOutline} from '@mui/icons-material'
 import StarIcon from '@mui/icons-material/Star';
-import data from './data'
-import { Box , TextField  , Typography } from '@mui/material'
+import { nearbyHotels } from './data'
+import { Box , TextField  , Typography , styled, Button } from '@mui/material'
 import {useHistory } from 'react-router-dom'
+import NavBar from '../navBar';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { Stack } from '@mui/system';
+
+
+const ActionTypes={
+  ADD_HOTELS: 'Add-hotels',
+  ERROR: 'error' ,
+  LOADING : 'loading',
+}
+
+
+
 function City() {
 const history = useHistory()
 const [MapMarkerLat, setMapMarkerLat] = useState('51.509865')
-const [PriceMin, setPriceMin] = useState(0)
-const [PriceMax, setPriceMax] = useState(1000000)
 const [MapMarkerLon, setMapMarkerLon] = useState('-0.118092')
 const [state, dispatch] = useReducer(reducer, {status: false , Hotels: []})
+
+
 const showPosition = (position) =>{
     // data(position.coords.latitude , position.coords.longitude).then(res =>{
    // })
 setMapMarkerLat(position.coords.latitude.toString() )
 setMapMarkerLon(position.coords.longitude.toString() )
 }
+// components
+
+const HeaderBox = styled(Box)()
+const HotelPage = styled(Box)(({theme})=>({
+  width : '100vw' , 
+  minHeight : '100vh' ,
+  display : 'flex' ,
+  alignItems : 'center' , 
+  justifyContent : 'start' ,
+  backgroundColor : theme.palette.primary.main ,
+  flexDirection : 'column'
+}))
+
+
+
+// const StyledTextField = styled(TextField)(({theme})=>({
+//   '& label.Mui-focused': {
+//     color: theme.palette.dark.dark,
+//   },
+//   '& .MuiInput-underline:after': {
+//     borderBottomColor: 'black',
+//   },
+//   '& .MuiOutlinedInput-root': {
+//     '& fieldset': {
+//       borderColor: theme.palette.dark.dark,
+//     },
+//     '&:hover fieldset': {
+//       borderColor: theme.palette.dark.dark,
+//     },
+//     '&.Mui-focused fieldset': {
+//       borderColor: theme.palette.dark.dark,
+//     },
+//   },
+//   backgroundColor : theme.palette.white.main,
+//    borderTopLeftRadius : theme.spacing(1) ,
+//    borderTopRightRadius : theme.spacing(1)
+// }));
+
+
+
+
+
+// functions
+
 function reducer (state , action){
     switch (action.type) {
-        case 'Add-hotels':  return ({ Hotels : action.Hotels , status : true })
-        case 'error' :  return ({  Hotels :[] , status : false})  ;   
-        case 'loading' :  return ({Hotels : ['loading'] , status : false })   
+        case ActionTypes.ADD_HOTELS:  return ({ Hotels : action.Hotels , status : action.type })
+        case  ActionTypes.ERROR :  return ({  Hotels :[] , status : action.type})  ;   
+        case  ActionTypes.LOADING :  return ({Hotels : [] , status : action.type })   
         default:  return ({ Hotels : [] , status :false })
     }
 }
+
+// functional components
+const HotelComponent = ({hotel})=>{
+  return <Stack sx={{padding : '16px' , backgroundColor: '#fff' , boxShadow :'2px 2px 4px black'  , borderRadius : '8px' }} spacing={2} >
+    <Typography variant="h4" textAlign="center" >{hotel.name}</Typography>
+    <Stack direction="row" spacing='80px' ><Typography>{hotel.region}</Typography><Typography>distance : {Number(hotel.distance).toFixed(2)}</Typography> </Stack>
+  </Stack>
+}
+
+
+function HotelsComponent (){
+return <Stack>
+{state.status === ActionTypes.LOADING && <Typography>Loading...</Typography> } 
+{state.status === ActionTypes.ADD_HOTELS && state.Hotels.length && <Stack spacing={1} >{state.Hotels.map(hotel =><HotelComponent hotel={hotel}/>)}</Stack>}      
+{state.status === ActionTypes.ADD_HOTELS && !state.Hotels.length && <Typography>There is no hotels near this place</Typography> }
+{state.sttatus === ActionTypes.ERROR && <Typography>Some Error Happened , Please Try Later</Typography>}
+</Stack>
+}
+
 const PositionError = (err)=>{
     // data().then(res =>{
     //     console.log(res)
@@ -42,20 +118,49 @@ function getLocation() {
    alert("Sorry ... ! this browser doesn't support location  ")
   } 
 }
-const getHotels = ()=>{
-  if(localStorage.getItem('hotelsData')){
-    dispatch({type: 'Add-hotels' , Hotels : JSON.parse( localStorage.getItem('hotelsData') ) })
+
+async function getHotels(){
+  dispatch({type: ActionTypes.LOADING})
+  const {data , error} = await nearbyHotels(MapMarkerLat , MapMarkerLon) ;
+  console.log(data)
+  if(data){
+  dispatch({type: ActionTypes.ADD_HOTELS , Hotels: data}) ;
   }
-else{
- dispatch({type: 'loading' , Hotels: [] })
-data(MapMarkerLat  , MapMarkerLon , PriceMax , PriceMin ).then(res =>{
-localStorage.setItem('hotelsData' , JSON.stringify( res.searchResults.results) )
-    dispatch({type: 'Add-hotels' , Hotels : res.searchResults.results })
-    }).catch(err =>{
-dispatch({type: 'error' , Hotels: []})
-    })
+  else if(error){
+  dispatch({type : ActionTypes.ERROR})
+  console.log(error)
   }
 }
+
+useEffect(() => {
+  
+getHotels()
+  return () => {
+  }
+}, [[MapMarkerLat , MapMarkerLon]])
+
+// const getHotels = ()=>{
+//   if(localStorage.getItem('hotelsData')){
+//     dispatch({type: 'Add-hotels' , Hotels : JSON.parse( localStorage.getItem('hotelsData') ) })
+//   }
+// else{
+//  dispatch({type: 'loading' , Hotels: [] })
+
+//  console.log("mapmarkerlat")
+//  console.log(MapMarkerLat)
+//  console.log(MapMarkerLon)
+//  console.log(PriceMax)
+//  console.log(PriceMin)
+// data(MapMarkerLat  , MapMarkerLon , PriceMax , PriceMin ).then(res =>{
+// localStorage.setItem('hotelsData' , JSON.stringify( res.searchResults.results) )
+//     dispatch({type: 'Add-hotels' , Hotels : res.searchResults.results })
+//     }).catch(err =>{
+//   console.log("error")
+//   console.log(err)
+// dispatch({type: 'error' , Hotels: []})
+//     })
+//   }
+// }
 const myLocation = ()=>{
     if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition , PositionError );
@@ -69,59 +174,86 @@ return <div className='ratingComponent' >
 { stars.map(item => {return <div> {item <= Math.floor(rate) ? <StarIcon/> :(  rate > item - 1 && Math.floor(rate) ==  item -1 ? <StarHalf/> : <StarOutline/> ) }  </div>} ) }
 </div>
 }
-const HotelsComponent =  useCallback( ()=>{
-if(state.status  && state.Hotels.length){
-return <div  className='hotelsComponent' > 
- { state.Hotels.map(item => <div className='HotelComponent'>
-<div className='info' > 
-<span>{item.address.countryName}</span>
-<span> {item.address.locality}</span>
-<span>{item.address.streetAddress}</span> 
-<span> {item.ratePlan.price.current}</span>
-</div>
-<div className='HotelThumbnail' style={{backgroundImage: `url(${item.optimizedThumbUrls.srpDesktop})`}}  > </div>
-<div className='footer' >
-<button onClick={()=>{history.push(`/hotelDetails/${item.id}`)}} >More Details </button>
-<RatingComponent rate={item.guestReviews.rating} />
-     </div>   
-</div> ) }</div> 
-}
-else if(state.status  && !state.Hotels.length) return  <div className='hotelErr'> <h1 > it seems that there  is no hotel in this place</h1></div>
-else if(!state.status &&  state.Hotels.length ) return (<div>this is the skeleton </div>)
-else if(!state.status && !state.Hotels.length) return <div className='hotelErr' > <h1  > error !! can't fetch the data  </h1></div>
-else return <h1>some error happened</h1>
-} , [state] )
+// const HotelsComponent =  useCallback( ()=>{
+// if(state.status  && state.Hotels.length){
+// return <div  className='hotelsComponent' > 
+//  { state.Hotels.map(item => <div className='HotelComponent'>
+// <div className='info' > 
+// <span>{item.address.countryName}</span>
+// <span> {item.address.locality}</span>
+// <span>{item.address.streetAddress}</span> 
+// <span> {item.ratePlan.price.current}</span>
+// </div>
+// <div className='HotelThumbnail' style={{backgroundImage: `url(${item.optimizedThumbUrls.srpDesktop})`}}  > </div>
+// <div className='footer' >
+// <button onClick={()=>{history.push(`/hotelDetails/${item.id}`)}} >More Details </button>
+// <RatingComponent rate={item.guestReviews.rating} />
+//      </div>   
+// </div> ) }</div> 
+// }
+// else if(state.status  && !state.Hotels.length) return  <div className='hotelErr'> <h1 > it seems that there  is no hotel in this place</h1></div>
+// else if(!state.status &&  state.Hotels.length ) return (<div>this is the skeleton </div>)
+// else if(!state.status && !state.Hotels.length) return <div className='hotelErr' > <h1  > error !! can't fetch the data  </h1></div>
+// else return <h1>some error happened</h1>
+// } , [state] )
 useEffect(() => {
   getLocation()
-   getHotels()
+  //  getHotels()
 }, [])
-return ( 
-<Box >
-<Typography>Hotels </Typography>
-<Box >
-      <Box > <GoogleMapReact
+    return <HotelPage>
+    <NavBar/>
+      <Typography variant='h3' margin={2} >Hotels</Typography>
+      <HeaderBox>
+<Box sx={{width : '90vw'  , height : '20vh' }} >
+<GoogleMapReact
         onClick={ (e)=>{  setMapMarkerLat(e.lat.toString()) ; setMapMarkerLon(e.lng.toString()) } }
        defaultCenter={{
       lat: Number(MapMarkerLat),
       lng: Number(MapMarkerLon)}}
           defaultZoom={11}
         >
-<Box  lat={Number(MapMarkerLat)}
-            lng={Number(MapMarkerLon)} >
+
+<Box  >
         </Box>
-        </GoogleMapReact></Box>
-        <Box className='parameters' >
-  <StarIcon onClick={myLocation} />
-  <Box variant='extended' onClick={getHotels} >
-Search Hotels
-  </Box>
-  <TextField type='number' value={PriceMin} onChange={(e)=>{setPriceMin(e.target.value)}} />
-  <TextField type='number' value={PriceMax} onChange={(e)=>{ setPriceMax(e.target.value) }} ></TextField>
-  </Box>
-  <Box>
-  <HotelsComponent/>
-</Box></Box></Box>
-    )
+        <LocationOnIcon lat={Number(MapMarkerLat)}
+            lng={Number(MapMarkerLon)}  />
+        </GoogleMapReact>    
+</Box>
+        <Box  sx={{display : 'flex' , alignItems: 'center' , justifyContent : 'space-around' , margin : '24px'}} >
+        </Box>
+      </HeaderBox>
+      <Box>
+      <HotelsComponent/>
+      </Box>
+    </HotelPage>
+//     return ( 
+// <Box >
+// <Typography>Hotels </Typography>
+// <Box >
+//       <Box > <GoogleMapReact
+//         onClick={ (e)=>{  setMapMarkerLat(e.lat.toString()) ; setMapMarkerLon(e.lng.toString()) } }
+//        defaultCenter={{
+//       lat: Number(MapMarkerLat),
+//       lng: Number(MapMarkerLon)}}
+//           defaultZoom={11}
+//         >
+// <Box  lat={Number(MapMarkerLat)}
+//             lng={Number(MapMarkerLon)} >
+//         </Box>
+//         </GoogleMapReact></Box>
+//         <Box className='parameters' >
+//   <StarIcon onClick={myLocation} />
+//   <Box variant='extended' onClick={getHotels} >
+// Search Hotels
+//   </Box>
+//   <TextField type='number' value={PriceMin} onChange={(e)=>{setPriceMin(e.target.value)}} />
+//   <TextField type='number' value={PriceMax} onChange={(e)=>{ setPriceMax(e.target.value) }} ></TextField>
+//   </Box>
+//   <Box>
+//   <HotelsComponent/>
+// </Box></Box></Box>
+//     )
+
 }
 
 export default City
